f0d3c0b696e95fbcfde21909af0d34e666474832
isocraze <97568527+isocraze@users.noreply.github.com>
2022-03-09 19:54:54 -0800

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,46 @@
+var _app = function () {
+    this.id = 0;
+    this.videoElement = null;
+    this.audioElement = null;
+    this.musicVolume = 0.12;
+    this.musicFadeIn = 4000;
+    this.skippedIntro = false;
+    this.backgroundToggler = false;
+    this.shouldIgnoreVideo = false;
+    this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
+    this.brandDescription = [
+        "trickster#6222 - 842568983525457940", 
+        "brickbattling since 2019 ", 
+        "all part of the plan ", 
+        "trickster's basement winning ",
+        "destroying the competition ",
+        "♫ Mobb Deep - Survival Of The Fittiest",
+        "$ trickster " ,
+    ];
+
+
+    this.iconChanger = function (urls, delay) {
+        if (!urls)
+            return;
+
+        delay = delay || 2000;
+
+        var counter = 0;
+        
+        setInterval(function () {
+            if(counter < urls.length) {
+                var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                link.type = 'image/x-icon';
+                link.rel = 'shortcut icon';
+                link.href = urls[counter];
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+            else 
+                counter = 0;
+
+           ++counter;
+        }, delay);
+    }
+};
+
+var app = new _app();

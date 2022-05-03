c85eef672fdb260f799dc03f1bd64e1810243cec
nTLOxVdAbEKGxyGOJZXjOrMPFdOAYLpuilrY <97568527+nTLOxVdAbEKGxyGOJZXjOrMPFdOAYLpuilrY@users.noreply.github.com>
2022-05-02 21:24:30 -0700

    Update portfolio.js

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -25,13 +25,22 @@
             },
 
                     {
-                name: 'v3rmillion',
+                name: 'v3rm',
                 link: 'https://v3rmillion.net/member.php?action=profile&uid=1429565'
             },
 
 		    {
                 name: 'roblox',
                 link: 'https://www.roblox.com/users/2742053608/profile'
+            },
+
+		    {
+                name: 'youtube',
+                link: 'https://www.youtube.com/channel/UCcQDVaXO5x2Nek8V1Fcfqeg'
+            },
+		    {
+                name: 'discord server',
+                link: 'https://discord.gg/g7bNB6FQMF'
             },
 
         ];
